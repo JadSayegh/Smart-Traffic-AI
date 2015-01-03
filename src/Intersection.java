a9aab680f2fe0925681fbac21e5aeaf26ac04c48
JadSayegh <jad.r.sayegh@gmail.com>
2015-01-03 17:58:38 +0200

    Intersection data structure

---
+++ src/Intersection.java
@@ -0,0 +1,15 @@
+
+
+
+public class Intersection {
+	int NScars, EWcars, x, y;
+	boolean isNSInput = false;
+	boolean isEWInput = false;
+	
+	public Intersection(int x, int y, int NScars, int EWcars){
+		this.x = x;
+		this.y = y;
+		this.NScars = NScars;
+		this.EWcars = EWcars;
+	}
+}
